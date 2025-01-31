d5daa8962be6dbe2fabce2351c908c7a2330d05a
Mojtaba Seyedi <seyedmojtabaseyedi@gmail.com>
2025-01-31 19:36:10 +0330

    init the project

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import vercelStatic from "@astrojs/vercel/static";
+
+// https://astro.build/config
+export default defineConfig({
+	output: "static",
+	adapter: vercelStatic(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
