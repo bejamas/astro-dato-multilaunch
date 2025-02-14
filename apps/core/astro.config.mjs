0e8ebb93f042a117d05a00d8b54bd81458c4d415
Mojtaba Seyedi <seyedmojtabaseyedi@gmail.com>
2025-02-15 00:21:50 +0330

    feat: init language switcher

--- apps/core/astro.config.mjs
+++ apps/core/astro.config.mjs
@@ -1,20 +1,20 @@
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
+import {defineConfig} from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
-	output: "static",
-	adapter: vercel(),
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	i18n: {
-		locales: ["en", "fr"],
-		defaultLocale: "en",
+  output: 'static',
+  adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  i18n: {
+    locales: ['en', 'fr', 'de', 'zh', 'ar'],
+    defaultLocale: 'en',
 
-		routing: {
-			prefixDefaultLocale: true, // Ensures that your default locale is prefixed aswell
-		},
-	},
-});
+    routing: {
+      prefixDefaultLocale: true, // Ensures that your default locale is prefixed aswell
+    },
+  },
+})
